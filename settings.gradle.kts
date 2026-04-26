2ac8eeb1e5af79c5ff8ad85e6bd18376d7e49b0f
najma <najmo-nur@tabaarakict.org>
2026-04-26 08:02:46 +0300

    Initial commit - HealthPath project

---
+++ settings.gradle.kts
@@ -0,0 +1,31 @@
+rootProject.name = "HealthPath"
+enableFeaturePreview("TYPESAFE_PROJECT_ACCESSORS")
+
+pluginManagement {
+    repositories {
+        google {
+            mavenContent {
+                includeGroupAndSubgroups("androidx")
+                includeGroupAndSubgroups("com.android")
+                includeGroupAndSubgroups("com.google")
+            }
+        }
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+
+dependencyResolutionManagement {
+    repositories {
+        google {
+            mavenContent {
+                includeGroupAndSubgroups("androidx")
+                includeGroupAndSubgroups("com.android")
+                includeGroupAndSubgroups("com.google")
+            }
+        }
+        mavenCentral()
+    }
+}
+
+include(":composeApp")
